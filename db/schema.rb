83101e2825814bef97b91078bc0f30518e6f2f7d
Rails udemy <rails@gmail.com>
2018-05-17 09:11:39 -0400

    add article and categories assosiaction

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517051942) do
+ActiveRecord::Schema.define(version: 20180517122312) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20180517051942) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "articles_categories", force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: true do |t|
